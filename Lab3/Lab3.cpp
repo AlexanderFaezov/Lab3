46538d87d4afc757d278c192dc75d3592e2c15c8
AlexanderFaezov <s.faezov.com@mail.ru>
2023-12-12 20:58:03 +0500

    Добавьте файлы проекта.

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int n = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - i; j++)
+        {
+            cout << ' ';
+        }
+        for (int j = n - 2 * i; j <= n; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
